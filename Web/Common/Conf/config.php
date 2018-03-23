abda804cc98ba525a9b848eb1957c68dfb7b950e
haohonglong <947923503@qq.com>
2018-03-23 14:58:27 +0800

    modify

--- Web/Common/Conf/config.php
+++ Web/Common/Conf/config.php
@@ -4,7 +4,7 @@ return array(
     'ACTION_SUFFIX' => 'Action', // 操作方法后缀
     //数据库
     'DB_TYPE' => 'mysql', // 数据库类型
-    'DB_HOST' => 'localhost', // 服务器地址
+    'DB_HOST' => '127.0.0.1', // 服务器地址
     'DB_NAME' => 'management_of_system', // 数据库名
     'DB_USER' => 'root', // 用户名
     'DB_PWD' => '', // 密码
