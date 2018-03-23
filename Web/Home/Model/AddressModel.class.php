0810f364b6d7d7d6edcb1a2c807d73dd06726038
haohonglong <947923503@qq.com>
2018-03-23 16:02:38 +0800

    modify

--- Web/Home/Model/AddressModel.class.php
+++ Web/Home/Model/AddressModel.class.php
@@ -11,25 +11,30 @@ class AddressModel extends CommonModel{
      * @return []
      */
     public function getAll() {
-        $arr=[];
+        $list=[];
         $sql = 'SELECT t1.id,t1.name,
                 t2.id AS addrId,t2.name AS addrName,t2.url,t2.info,t2.date,t2.sortsId
                 FROM sorts AS t1
                 LEFT JOIN video_addres AS t2 ON t1.id=t2.sortsId';
         $result= $this->query($sql);
-//        dump($result);exit;
-        foreach ($result as $key => $value) {
-            $arr[$value['id']]['sortId']=$value['id'];
-            $arr[$value['id']]['sortName']=$value['name'];
-            $arr[$value['id']]['sorts'][$key]['id']=$value['addrid'];
-            $arr[$value['id']]['sorts'][$key]['urlTitle']=$value['addrname'];
-            $arr[$value['id']]['sorts'][$key]['url']=$value['url'];
-            $arr[$value['id']]['sorts'][$key]['info']=$value['info'];
-            $arr[$value['id']]['sorts'][$key]['date']=$value['date'];
-            $arr[$value['id']]['sorts'][$key]['sortsId']=$value['sortsid'];
-            $arr[$value['id']]['sorts']=array_values($arr[$value['id']]['sorts']);
+        foreach ($result as $key => $item){
+            if(!isset($list[$item['id']])){
+                $list[$item['id']] = [
+                    'sortId'=>$item['id'],
+                    'sortName'=>$item['name'],
+                    'sorts'=>[],
+                ];
+            }
+            $list[$item['id']]['sorts'][] = [
+                'id'=>$item['addrid'],
+                'urlTitle'=>$item['addrname'],
+                'url'=>$item['url'],
+                'info'=>$item['info'],
+                'date'=>$item['date'],
+            ];
+
         }
-        return $arr;
+        return $list;
     }
 
 }
